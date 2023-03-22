da542baf2b3960b676fb81fbe19e5d75ed2dae32
Andrew Kuhs <andythekuhs@gmail.com>
2023-03-22 01:42:49 -0400

    added text to body

--- main.py
+++ main.py
@@ -7,10 +7,25 @@ filepaths = glob.glob("files/*.txt")
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
 for filepath in filepaths:
+
+    # Add a page to the pdf document for each text file
     pdf.add_page()
+
+    # Get the filename without extension
+    # and convert to title case
     filename = Path(filepath).stem
     title = filename.split(".")[0].title()
+
+    # Add the name to the pdf
     pdf.set_font(family="Times", size=24, style="B")
     pdf.cell(w=0, h=12, txt=title, align="L", ln=1)
+
+    # Get the content of each text file
+    with open(filepath, "r") as file:
+        content = file.read()
+
+    # Add the text file content to the pdf
+    pdf.set_font(family="Times", size=12)
+    pdf.multi_cell(w=0, h=6, txt=content)
 
 pdf.output("output.pdf")
